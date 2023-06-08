421864371e5fc522af8dc556fb2d0cc6128f074f
Андрей <110286901+Andrey262241@users.noreply.github.com>
2023-06-08 20:35:31 +0300

    1

---
+++ src/js/03-promises.js
@@ -0,0 +1,43 @@
+import Notiflix from 'notiflix';
+
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', promissGenerator)
+
+function promissGenerator(e) {
+  e.preventDefault();
+  let delay = Number(firstDelay.value);
+  for (let position = 1; position <= amount.value; position++){
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+      Notiflix.Notify.success(
+          `Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${position} in ${delay}ms`
+        );
+      })
+    delay += Number(delayStep.value);
+  }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+       if (shouldResolve) {
+         resolve({ position, delay });
+  } else {
+        reject({position, delay})
+  }
+    }, delay)
+  })
+ 
+}
